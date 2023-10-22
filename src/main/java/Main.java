b93ee78b0c3cad0521faf8802f030e87976badf3
Valery Chyrok <vakachy@gmail.com>
2023-10-22 23:54:52 +0300

    Added Homework_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+/*
+Создать справочник сотрудников
+Необходимо:
+Создать класс справочник сотрудников, который содержит внутри коллекцию сотрудников - каждый сотрудник должен иметь следующие атрибуты:
+Табельный номер
+Номер телефона
+Имя
+Стаж
+Добавить метод, который ищет сотрудника по стажу (может быть список)
+Добавить метод, который выводит номер телефона сотрудника по имени (может быть список)
+Добавить метод, который ищет сотрудника по табельному номеру
+Добавить метод добавление нового сотрудника в справочник
+*/
 public class Main {
     public static void main(String[] args) {
         Phonebook pb = new Phonebook("Employee's Phonebook");
         pb.addEmployee(Employee.createEmployee("John", "9916897", "10"));
         pb.addEmployee(Employee.createEmployee("Mike", "9999999", "1"));
-        pb.addEmployee(Employee.createEmployee( "Ольга", "1111111", "12"));
-        pb.addEmployee(Employee.createEmployee( "Василий", "5559877", "0"));
-        pb.addEmployee(Employee.createEmployee( "Steeve", "1212355", "10"));
-        pb.addEmployee(Employee.createEmployee( "Mike", "9009090", "15"));
+        pb.addEmployee(Employee.createEmployee("Ольга", "1111111", "12"));
+        pb.addEmployee(Employee.createEmployee("Василий", "5559877", "0"));
+        pb.addEmployee(Employee.createEmployee("Steeve", "1212355", "10"));
+        pb.addEmployee(Employee.createEmployee("Mike", "9009090", "15"));
 
         pb.printPhonebook();
         System.out.println("***");
